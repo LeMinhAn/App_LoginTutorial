bb70998ae7c915bda7d5fe2fdbb964eb72be371d
LeMinhAn <leminhan.cst@gmail.com>
2016-06-10 17:36:27 +0700

    Commit add HomeActivity

--- app/src/main/java/leminhan/logintutorial/HomeActivity.java
+++ app/src/main/java/leminhan/logintutorial/HomeActivity.java
@@ -1,13 +1,69 @@
 package leminhan.logintutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import leminhan.logintutorial.flowingdrawer_core.FlowingView;
+import leminhan.logintutorial.flowingdrawer_core.LeftDrawerLayout;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private RecyclerView rvFeed;
+    private LeftDrawerLayout mLeftDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupToolbar();
+        mLeftDrawerLayout = (LeftDrawerLayout) findViewById(R.id.id_drawerlayout);
+        rvFeed = (RecyclerView) findViewById(R.id.rvFeed);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MyMenuFragment mMenuFragment = (MyMenuFragment) fm.findFragmentById(R.id.id_container_menu);
+        FlowingView mFlowingView = (FlowingView) findViewById(R.id.sv);
+        if (mMenuFragment == null) {
+            fm.beginTransaction().add(R.id.id_container_menu, mMenuFragment = new MyMenuFragment()).commit();
+        }
+        mLeftDrawerLayout.setFluidView(mFlowingView);
+        mLeftDrawerLayout.setMenuFragment(mMenuFragment);
+        setupFeed();
+    }
+    protected void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLeftDrawerLayout.toggle();
+            }
+        });
+    }
+    private void setupFeed() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this) {
+            @Override
+            protected int getExtraLayoutSpace(RecyclerView.State state) {
+                return 300;
+            }
+        };
+        rvFeed.setLayoutManager(linearLayoutManager);
+
+        FeedAdapter feedAdapter = new FeedAdapter(this);
+        rvFeed.setAdapter(feedAdapter);
+        feedAdapter.updateItems();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mLeftDrawerLayout.isShownMenu()) {
+            mLeftDrawerLayout.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
